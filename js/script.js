fe1d778e6ed36a10bdd8ada4155ae247f3f350d4
Yixin Lin <yixin1996@gmail.com>
2015-03-22 18:00:47 -0400

    more visual changes

--- js/script.js
+++ js/script.js
@@ -20,27 +20,67 @@ $('body').vegas({
 
 // Nicescroll
 
+
 // $(document).ready(function() {  
 // 	$("html").niceScroll();
 // });
 
 
 // Navbar effects
+var navbarHeight = $('.navbar').offset()['top'];
 
 $(document).ready(function() {
 	$('.navbar-brand').click(function(e) {
 		e.preventDefault();
 		$('.nav-disappear:visible').hide("slow");
 		$('.nav-disappear:hidden').show("fast");
+		$('.nav-appear:visible').hide("slow");
+		$('.nav-appear:hidden').show("fast");
 	});
 
 	$(document).scroll(function() {
 		var scroll = $(this).scrollTop();
-		if (scroll>0) {
+		if (scroll>navbarHeight) {
 			$('.nav-disappear').hide("slow");
+			$('.nav-appear').show("fast");
 		}
 		else {
 			$('.nav-disappear').show("fast");
+			$('.nav-appear').hide("slow");
 		}
+	});
+});
+
+$(window).bind('scroll', function() {
+	// Sticky scroll
+	var currHeight = $(window).scrollTop();
+	if ( currHeight > navbarHeight) {
+		$('.navbar').addClass('navbar-fixed-top');
+	}
+	else {
+		$('.navbar').removeClass('navbar-fixed-top');
+
+
+
+		// Variable opacity based on location relative to navbar
+		$('.background-overlay').css({"opacity": 0.5 + 0.8*(1 - ((navbarHeight - currHeight) / navbarHeight))/2 })
+	}
+});
+
+// smooth scroll for "hello" button
+$(function() {
+	$('.jumbotron a').click(function() {
+
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+
 	});
 });
