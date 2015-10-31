47845105c26273fb30ef7ade8aa872c559b5807c
1heart <yixin1996@gmail.com>
2015-10-31 17:07:07 -0400

    majorly wip

---
+++ src/App.js
@@ -0,0 +1,15 @@
+var CommentBox = React.createClass({
+	render: function() {
+		return (
+			<div className="commentBox">
+				Hello, world!
+			</div>	
+		);
+	}
+});
+
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
